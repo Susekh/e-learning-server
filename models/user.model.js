760346f12e605c5f5f800024cf4f7faef5d1f46e
Subh <subhranshukhilar@outlook.com>
2025-04-08 00:53:16 +0530

    fixed the null for regNo in user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,49 +1,57 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const generateRegNo = () => {
+  return Math.floor(Math.random() * 1000000).toString();
+};
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    regNo : {
-        type : String,
-        require : false,
-        unique : true
+    regNo: {
+      type: String,
+      required: false,
+      unique: true,
+      default: generateRegNo,
     },
     adminId: {
-        type: String,
-        required: false,
-        default: function() {
-            
-            if (this.role === "admin") {
-                return this._id.toString();  
-            }
-            return null;  
+      type: String,
+      required: false,
+      default: function () {
+        if (this.role === "admin") {
+          return this._id.toString();
         }
+        return null;
+      },
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+      type: String,
+      required: true,
     },
-    role:{
-        type:String,
-        enum:["admin","instructor", "student"],
-        default:'admin'
+    role: {
+      type: String,
+      enum: ["admin", "instructor", "student"],
+      default: "admin",
     },
-    enrolledCourses:[
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:'Course'
-        }
+    enrolledCourses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
     ],
-    photoUrl:{
-        type:String,
-        default:""
-    }
-},{timestamps:true});
+    photoUrl: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
 
 export const User = mongoose.model("User", userSchema);
